327b6ae739018b8dc24a0dbfb601b5ba2a7fe6b5
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2022-01-27 11:26:11 +0100

    Store logger, not entire context.

--- wasm/kibana_plugin/src/server/plugin/plugin.rs
+++ wasm/kibana_plugin/src/server/plugin/plugin.rs
@@ -1,13 +1,13 @@
 use super::route_handlers::RouteHandlers;
 use crate::server::plugin::{PluginSetup, PluginStart};
 use kibana_core_types::server::{
-    packages::kbn_i18n, CoreSetup, PluginInitializerContext, ResponseOptions, RouteConfig,
+    packages::kbn_i18n, CoreSetup, Logger, PluginInitializerContext, ResponseOptions, RouteConfig,
 };
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct Plugin {
-    context: PluginInitializerContext,
+    logger: Logger,
     route_handlers: RouteHandlers,
 }
 
@@ -16,32 +16,28 @@ impl Plugin {
     #[wasm_bindgen(constructor)]
     pub fn new(context: PluginInitializerContext) -> Self {
         Self {
-            context,
+            logger: context.logger().get_with_context("wasm"),
             route_handlers: RouteHandlers::new(),
         }
     }
 
     pub fn setup(&mut self, core: CoreSetup) -> Result<PluginSetup, JsValue> {
-        self.context
-            .logger()
-            .get_with_context("wasm")
-            .debug("Setting up plugin.");
+        self.logger.debug("Setting up plugin.");
 
         let router = core.http().create_router();
         router.post(
             RouteConfig::new("/api/wasm"),
             self.route_handlers.create_handler(|_, _, res| {
-                kbn_i18n::translate(
-                    "exampleRs.welcomeMessage",
-                    kbn_i18n::I18nParams::new("Welcome {name}!".to_string())
-                        .with_values([("name", "Kibana")].iter().cloned().collect()),
+                let i18n_params = kbn_i18n::I18nParams::new("Welcome {name}!".to_string())
+                    .with_values([("name", "Kibana")].iter().cloned().collect());
+                kbn_i18n::translate("exampleRs.welcomeMessage", i18n_params).map(
+                    |translated_string| {
+                        res.ok_with_options(ResponseOptions::with_body(format!(
+                            "Response: {}.",
+                            translated_string
+                        )))
+                    },
                 )
-                .map(|translated_string| {
-                    res.ok_with_options(ResponseOptions::with_body(format!(
-                        "Response: {}.",
-                        translated_string
-                    )))
-                })
             }),
         );
 
@@ -49,10 +45,7 @@ impl Plugin {
     }
 
     pub fn start(&self) -> Result<PluginStart, JsValue> {
-        self.context
-            .logger()
-            .get_with_context("wasm")
-            .debug("Starting plugin.");
+        self.logger.debug("Starting plugin.");
         Ok(PluginStart::new())
     }
 }
